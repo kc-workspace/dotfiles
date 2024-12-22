c48b106ec81cecfbbe9f335b2df7527a6fad8035
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2024-12-22 23:27:57 +0700

    perf(script): support check the result within setup command
    Signed-off-by: Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>

--- scripts/setup-full.sh
+++ scripts/setup-full.sh
@@ -7,7 +7,7 @@ echo "# Initiate chezmoi with full-version mode
 On this mode, we will configure 1password service-account for decrypt file.
 "
 
-: "${CHEZMOI_ARGUMENTS:=--apply}"
+: "${CHEZMOI_ARGUMENTS:=}"
 : "${OP_SERVICE_ACCOUNT_TOKEN:=${OP_TOKEN:-${TOKEN}}}"
 export OP_ACCOUNT="my.1password.com"
 export OP_SERVICE_ACCOUNT_TOKEN
@@ -25,10 +25,46 @@ if [ -z "$OP_SERVICE_ACCOUNT_TOKEN" ]; then
   echo
 fi
 
-set -x
+_run() {
+  local mode="$1" name="$2"
+  shift 2
 
-## Check is 1password should already login for full mode
-op whoami
+  local stdout stderr status
+  stdout="$(mktemp)"
+  stderr="$(mktemp)"
+
+  printf '$ Run "%s": ' "$name"
+  if "$@" >"$stdout" 2>"$stderr"; then
+    status=0
+  else
+    status=1
+  fi
+
+  if [ $status -eq 1 ] && [[ $mode == required ]]; then
+    status=2
+  fi
+
+  case "$status" in
+  0) printf 'PASSED\n' ;;
+  1) printf 'FAILED (OPTIONAL)\n' ;;
+  2) printf 'FAILED\n' ;;
+  esac
+
+  if [ -s "$stdout" ]; then
+    printf '  - OUTPUT::STDOUT\n'
+    cat "$stdout"
+  fi
+  if [ -s "$stderr" ]; then
+    printf '  - OUTPUT::STDERR\n'
+    cat "$stderr"
+  fi
+
+  echo
+  rm "$stdout" "$stderr"
+}
+
+_run required "check 1password account" \
+  op whoami
 
 GPG_FINGERPRINT="$(op item get \
   "$GPG_KEY_OP_ITEM" \
@@ -43,10 +79,26 @@ op document get \
 gpg --quiet --import "$GPG_PATH"
 rm "$GPG_PATH"
 
-## Apply chezmoi configuration
 # shellcheck disable=SC2086
-chezmoi init $CHEZMOI_ARGUMENTS
+_run required "apply chezmoi configuration" \
+  chezmoi init --apply $CHEZMOI_ARGUMENTS
+# shellcheck disable=SC2086
+_run optional "check chezmoi status" \
+  chezmoi status $CHEZMOI_ARGUMENTS
+# shellcheck disable=SC2086
+_run optional "check chezmoi doctor" \
+  chezmoi doctor $CHEZMOI_ARGUMENTS
+# shellcheck disable=SC2086
+_run optional "check chezmoi path" \
+  chezmoi managed --path-style absolute --tree $CHEZMOI_ARGUMENTS
+
+temp="$(mktemp)" code=0
+# shellcheck disable=SC2086
+chezmoi status $CHEZMOI_ARGUMENTS | tee "$temp"
+test -s "$temp" && code=1
 
 if [ -n "$GPG_FINGERPRINT" ]; then
   gpg --batch --yes --delete-secret-and-public-key "$GPG_FINGERPRINT"
 fi
+
+exit $code
--- scripts/setup-minimal.sh
+++ scripts/setup-minimal.sh
@@ -7,13 +7,65 @@ echo "# Initiate chezmoi with minimal mode
 On this mode, we will removed all encrypted file.
 "
 
-: "${CHEZMOI_ARGUMENTS:=--apply}"
+: "${CHEZMOI_ARGUMENTS:=}"
 export KCDF_MODE=minimal
 
-set -x
+_run() {
+  local mode="$1" name="$2"
+  shift 2
+
+  local stdout stderr status
+  stdout="$(mktemp)"
+  stderr="$(mktemp)"
+
+  printf '$ Run "%s": ' "$name"
+  if "$@" >"$stdout" 2>"$stderr"; then
+    status=0
+  else
+    status=1
+  fi
+
+  if [ $status -eq 1 ] && [[ $mode == required ]]; then
+    status=2
+  fi
+
+  case "$status" in
+  0) printf 'PASSED\n' ;;
+  1) printf 'FAILED (OPTIONAL)\n' ;;
+  2) printf 'FAILED\n' ;;
+  esac
+
+  if [ -s "$stdout" ]; then
+    printf '  - OUTPUT::STDOUT\n'
+    cat "$stdout"
+  fi
+  if [ -s "$stderr" ]; then
+    printf '  - OUTPUT::STDERR\n'
+    cat "$stderr"
+  fi
+
+  echo
+  rm "$stdout" "$stderr"
+}
 
 ## Delete all asc file
+printf '$ find %s -name "*.asc" -exec rm {} \;\n' "$CHEZMOI_HOME"
 find "${CHEZMOI_HOME:?}" -name "*.asc" -exec rm {} \;
-## Apply chezmoi configuration
+
 # shellcheck disable=SC2086
-chezmoi init $CHEZMOI_ARGUMENTS
+_run required "apply chezmoi configuration" \
+  chezmoi init --apply $CHEZMOI_ARGUMENTS
+# shellcheck disable=SC2086
+_run optional "check chezmoi status" \
+  chezmoi status $CHEZMOI_ARGUMENTS
+# shellcheck disable=SC2086
+_run optional "check chezmoi doctor" \
+  chezmoi doctor $CHEZMOI_ARGUMENTS
+# shellcheck disable=SC2086
+_run optional "check chezmoi path" \
+  chezmoi managed --path-style absolute --tree $CHEZMOI_ARGUMENTS
+
+temp="$(mktemp)"
+# shellcheck disable=SC2086
+chezmoi status $CHEZMOI_ARGUMENTS | tee "$temp"
+test -s "$temp" && exit 1
