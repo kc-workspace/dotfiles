a95cc59018635216d103a4f5b8556a8fefd03c13
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2024-12-24 00:01:10 +0700

    feat(script): add verify-chezmoi.sh to check the init & apply results
    Signed-off-by: Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>

---
+++ home/dot_local/bin/executable_verify-chezmoi.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+## run mise path to ensure tools are available
+eval "$(mise activate bash)"
+
+set -e
+set -x
+
+## Verify commands
+printf 'mise: ' && command -v mise
+printf 'chezmoi: ' && command -v chezmoi
+printf 'zsh: ' && command -v zsh
+
+## Verify chezmoi
+temp="$(mktemp)"
+# shellcheck disable=SC2086
+chezmoi status --no-pager --no-tty $CHEZMOI_ARGUMENTS | tee "$temp"
+if ! [ -s "$temp" ]; then
+  printf 'chezmoi should be on the clean state' >&2
+  exit 2
+fi
+
+## Verify zsh version
+printf 'zsh version: '
+zsh -c 'echo "${ZSH_VERSION:?}"'
+
+## Verify 'lsd' command
+printf 'list directory: '
+zsh -c 'lsd $HOME'
+
+## Verify mise command
+mise doctor
