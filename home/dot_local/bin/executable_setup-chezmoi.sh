450478d4c4756afe2d414eb6e5c00b477a7ffcdc
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2024-12-23 23:24:44 +0700

    perf(script): improve setup-* and cleanup-* script
    Signed-off-by: Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>

--- home/dot_local/bin/executable_setup-chezmoi.sh
+++ home/dot_local/bin/executable_setup-chezmoi.sh
@@ -2,19 +2,19 @@
 
 set -e
 
-if [ -z "$GITHUB_TOKEN" ] ||
-  [ -z "$OP_SERVICE_ACCOUNT_TOKEN" ] ||
-  [ -z "$GPG_FINGERPRINT" ]; then
-  pushd "$(dirname "$0")"
-  # shellcheck disable=SC1091
-  source "$PWD/setup-secrets.sh"
-  popd
-fi
+pushd "$(dirname "$0")"
 
+# shellcheck disable=SC1091
+source "$PWD/setup-secrets.sh"
+
+printf "\n==============================================
+Initiate and Apply %s
+==============================================\n" "chezmoi config"
 # shellcheck disable=SC2086
 chezmoi init --apply --no-pager --no-tty $CHEZMOI_ARGUMENTS
 
-pushd "$(dirname "$0")"
 # shellcheck disable=SC1091
 source "$PWD/cleanup-secrets.sh"
+
+set +x
 popd
