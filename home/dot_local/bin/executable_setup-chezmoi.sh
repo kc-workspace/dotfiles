5f404d67ac3af8151ce3da45ee9e2d4374fdd785
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2024-12-24 00:25:01 +0700

    chore: save progress
    Signed-off-by: Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>

--- home/dot_local/bin/executable_setup-chezmoi.sh
+++ home/dot_local/bin/executable_setup-chezmoi.sh
@@ -2,18 +2,21 @@
 
 set -e
 
-if [ -z "$GITHUB_TOKEN" ] ||
-  [ -z "$OP_SERVICE_ACCOUNT_TOKEN" ] ||
-  [ -z "$GPG_FINGERPRINT" ]; then
-  pushd "$(dirname "$0")"
-  # shellcheck disable=SC1091
-  source "$PWD/setup-secrets.sh"
-  popd
-fi
-
-chezmoi init --apply --no-pager --no-tty
-
 pushd "$(dirname "$0")"
+# shellcheck disable=SC1091
+source "$PWD/setup-secrets.sh"
+
+printf "\n==============================================
+Initiate and Apply %s
+==============================================\n" "chezmoi config"
+# shellcheck disable=SC2086
+chezmoi init --apply --no-pager --no-tty $CHEZMOI_ARGUMENTS
+
+printf "\n==============================================
+Install %s
+==============================================\n" "mise tools"
+mise install
+
 # shellcheck disable=SC1091
 source "$PWD/cleanup-secrets.sh"
 popd
---
+++ home/dot_local/bin/executable_verify-chezmoi.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+set -e
+
+## run mise path to ensure tools are available
+eval "$(mise activate bash)"
+
+printf "\n==============================================
+Verify %s
+==============================================\n" "commands"
+printf 'curl: ' && command -v curl
+printf 'git: ' && command -v git
+printf 'unzip: ' && command -v unzip
+printf 'mise: ' && command -v mise
+printf 'chezmoi: ' && command -v chezmoi
+printf 'op: ' && command -v op
+printf 'gpg: ' && command -v gpg
+printf 'zsh: ' && command -v zsh
+
+printf "\n==============================================
+Verify %s
+==============================================\n" "chezmoi"
+temp="$(mktemp)"
+# shellcheck disable=SC2086
+chezmoi status --no-pager --no-tty $CHEZMOI_ARGUMENTS | tee "$temp"
+if [ -s "$temp" ]; then
+  printf 'chezmoi should be on the clean state' >&2
+  exit 2
+fi
+
+printf "\n==============================================
+Verify %s
+==============================================\n" "zsh"
+printf 'zsh version: '
+zsh -c 'echo "${ZSH_VERSION:?}"'
+
+printf "\n==============================================
+Verify %s
+==============================================\n" "mise tools"
+mise list
+
+printf "\n==============================================
+Verify %s
+==============================================\n" "mise health"
+mise doctor
