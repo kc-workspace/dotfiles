207731001379f47ac89e27cd7d7f31cf2b3e167e
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2025-06-15 11:43:46 +0700

    perf(core): update source state change
    Files updated:
    Update home/.config/wezterm/wezterm.lua

--- home/dot_config/wezterm/wezterm.lua
+++ home/dot_config/wezterm/wezterm.lua
@@ -26,4 +26,67 @@ config.keys = {
   },
 }
 
+-- Events --
+
+local title_patterns = {
+  {
+    title    = "[PS] {relpath}",
+    prefixes = {
+      "~/Desktop/Personal"
+    }
+  },
+  {
+    title    = "[RX] {relpath}",
+    prefixes = {
+      "~/Desktop/Works/Rx"
+    }
+  },
+  {
+    title    = "[ST] {relpath}",
+    prefixes = {
+      "~/Desktop/Works/Smartertravel",
+      "~/Desktop/Works/Smartertravel.old"
+    }
+  },
+  {
+    title    = "[TMP] {relpath}",
+    prefixes = {
+      "/tmp",
+    }
+  }
+}
+
+wezterm.on('format-tab-title', function(tab, tabs, panes, config, hover, max_width)
+  local uri = tostring(tab.active_pane.current_working_dir)
+
+  if uri == "nil" then
+    wezterm.log_info('Use pane title: ' .. tab.active_pane.title)
+    return tab.active_pane.title
+  end
+
+  local home = os.getenv("HOME")
+  local cwd = uri
+    :gsub("^file://", "")
+    :gsub(home, "~")
+    :gsub("^/private", "")
+    :gsub("%%(%x%x)", function(hex)
+      return string.char(tonumber(hex, 16))
+    end)
+
+  -- Matching logic
+  for _, group in ipairs(title_patterns) do
+    for _, prefix in ipairs(group.prefixes) do
+      if cwd:sub(1, #prefix) == prefix then
+        local suffix = cwd:sub(#prefix + 2) -- remove / prefix
+        local relpath = (suffix == "") and "<root>" or suffix
+        wezterm.log_info('Use title template: ' .. group.title)
+        return group.title:gsub("{relpath}", relpath)
+      end
+    end
+  end
+
+  wezterm.log_info('Use cwd title: ' .. cwd)
+  return cwd
+end)
+
 return config
