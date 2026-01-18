ba160d893c8cea144c16505b02eea2842e313a35
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2026-01-18 15:29:28 +0700

    feat: set XDG_* for zsh for better cross-platform support
    Signed-off-by: Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>

---
+++ home/dot_config/zsh/environments.zsh
@@ -0,0 +1,5 @@
+## https://gist.github.com/roalcantara/107ba66dfa3b9d023ac9329e639bc58c
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_BIN_HOME="$HOME/.local/bin"
+export XDG_CACHE_HOME="$HOME/.cache"
