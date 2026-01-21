c7d157b084608af4c67b10d8cd183ea73feca265
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2026-01-21 12:49:03 +0700

    fix: history-substring didn't works when zvim is enabled
    Signed-off-by: Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>

--- home/.chezmoitemplates/zshrc/app/history-substring.zsh
+++ home/.chezmoitemplates/zshrc/app/history-substring.zsh
@@ -1,14 +1,16 @@
 {{- if not (get . "disabled") }}
 {{- template "zshrc/helpers/h3" "Zinit Plugins - history substring" }}
 
+{{- $keymap := get . "keymap" | default "" }}
+
 ## Setup substring history search
 ## https://github.com/zsh-users/zsh-history-substring-search
 zinit ice {{- if hasKey . "lazy" | ternary (get . "lazy") false }} wait lucid{{- end }} atinit"
 export HISTORY_SUBSTRING_SEARCH_PREFIXED=true
 export HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_TIMEOUT=3
 " atload"
-bindkey '^[[A' history-substring-search-up
-bindkey '^[[B' history-substring-search-down"
+bindkey {{- if $keymap }} -M {{ $keymap }}{{ end }} '^[[A' history-substring-search-up
+bindkey {{- if $keymap }} -M {{ $keymap }}{{ end }} '^[[B' history-substring-search-down"
 zinit {{ get . "act" | default "light" }} zsh-users/zsh-history-substring-search
 
 {{ end -}}
