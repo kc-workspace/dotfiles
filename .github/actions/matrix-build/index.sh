2603a6181b9ab24d5a2ba6d481627f35e68a0892
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2026-01-22 11:00:12 +0700

    save changes
    Signed-off-by: Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>

--- .github/actions/matrix-build/index.sh
+++ .github/actions/matrix-build/index.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env sh
 
-echo 'matrix<<EOF'
-echo '{'
-echo '  "include": ['
-echo '    { "runs-on": "ubuntu-24.04", "image": "ubuntu:latest", "platform": "linux/amd64" },'
-echo '    { "runs-on": "ubuntu-24.04-arm", "image": "ubuntu:latest", "platform": "linux/arm64" },'
-echo '    { "runs-on": "ubuntu-24.04-arm", "image": "debian:latest", "platform": "linux/arm64" }'
-echo '  ]'
-echo '}'
-echo EOF
+env
+
+{
+  echo 'matrix<<EOF'
+  echo '{'
+  echo '  "include": ['
+  echo '    { "runs-on": "ubuntu-24.04", "image": "ubuntu:latest", "platform": "linux/amd64" },'
+  echo '    { "runs-on": "ubuntu-24.04-arm", "image": "ubuntu:latest", "platform": "linux/arm64" },'
+  echo '    { "runs-on": "ubuntu-24.04-arm", "image": "debian:latest", "platform": "linux/arm64" }'
+  echo '  ]'
+  echo '}'
+  echo EOF
+} >>"$GITHUB_OUTPUT"
