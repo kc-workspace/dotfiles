782c61da2e7ad5c2a067a86bfaba82bc8497b528
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2026-01-22 10:40:49 +0700

    ci: try
    Signed-off-by: Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>

--- .github/actions/matrix-build/index.sh
+++ .github/actions/matrix-build/index.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env sh
 
-echo "matrix=goodbye"
+echo 'matrix<<EOF'
+echo '{'
+echo '  "include": ['
+echo '    { "runs-on": "ubuntu-24.04", "image": "ubuntu:latest", "platform": "linux/amd64" },'
+echo '    { "runs-on": "ubuntu-24.04-arm", "image": "ubuntu:latest", "platform": "linux/arm64" }'
+echo '    { "runs-on": "ubuntu-24.04-arm", "image": "debian:latest", "platform": "linux/arm64" }'
+echo '  ]'
+echo '}'
+echo EOF
