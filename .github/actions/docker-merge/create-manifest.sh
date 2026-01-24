da1178070a6536d4318cbd0f27edf4af4d2d5b0e
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2026-01-24 10:39:33 +0700

    ci: attest provenance on final manifest instead of individual image on each platform
    Signed-off-by: Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>

--- .github/actions/docker-merge/create-manifest.sh
+++ .github/actions/docker-merge/create-manifest.sh
@@ -30,6 +30,13 @@ inspect_manifest() {
   docker "${args[@]}"
 }
 
+get_manifest_digest() {
+  local image_name="$1" image_version="$2"
+  docker buildx imagetools inspect \
+    "$image_name:$image_version" \
+    --format '{{ println .Manifest.Digest }}'
+}
+
 main() {
   local image_name="$1" image_version="$2"
   local metadata="$RUNNER_TEMP/metadata.json"
@@ -37,6 +44,12 @@ main() {
   echo "$DOCKER_METADATA_OUTPUT_JSON" >"$metadata"
   create_manifest "$metadata" "$image_name"
   inspect_manifest "$image_name" "$image_version"
+
+  {
+    echo "subject=$image_name"
+    printf "digest="
+    get_manifest_digest "$image_name" "$image_version"
+  } | tee "$GITHUB_OUTPUT"
 }
 
 : "${DIGEST_PREFIX:?}"
@@ -44,6 +57,7 @@ main() {
 : "${IMAGE_VERSION:?}"
 : "${DOCKER_METADATA_OUTPUT_JSON:?}"
 : "${RUNNER_TEMP:?}"
+: "${GITHUB_OUTPUT:?}"
 
 cd "$RUNNER_TEMP/$DIGEST_PREFIX"
 main "$IMAGE_NAME" "$IMAGE_VERSION"
