5ce9d6eba8a3f0e5c349b93d924c7a2ca94def08
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2026-01-24 22:32:14 +0700

    fix(zsh): load different starship theme when using copilot
    Signed-off-by: Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>

--- home/.chezmoitemplates/zshrc/app/starship.zsh
+++ home/.chezmoitemplates/zshrc/app/starship.zsh
@@ -1,6 +1,12 @@
 {{- if not (get . "disabled") }}
 {{- template "zshrc/helpers/h3" "Zinit Plugins - Starship theme" }}
 
+## On our case is the right_format that cause the problem.
+## https://github.com/microsoft/vscode/issues/285926
+if [[ -n "$COPILOT" ]]; then
+  export STARSHIP_CONFIG="$HOME/.config/starship-copilot.toml"
+fi
+
 ## Setup starship prompt
 ## https://starship.rs
 zinit ice {{- if hasKey . "lazy" | ternary (get . "lazy") false }} wait lucid{{- end }} as"command" from"gh-r" \
