d1035f5ded80773d17757d3c862828fe916be858
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2026-06-03 15:49:02 +0700

    perf(zsh): support install neovim from mise additional to zinit
    Signed-off-by: Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>

--- home/.chezmoitemplates/zshrc/app/neovim.zsh
+++ home/.chezmoitemplates/zshrc/app/neovim.zsh
@@ -1,4 +1,7 @@
 {{- if not (get . "disabled") }}
+{{- $method := dig "method" "zinit" . }}
+{{- if eq $method "zinit" }}
+
 {{- template "zshrc/helpers/h3" "Zinit Plugins - neovim" }}
 
 ## Setup nvim - vim alternative command
@@ -9,6 +12,15 @@ zinit ice {{- if hasKey . "lazy" | ternary (get . "lazy") true }} wait lucid{{- 
 export EDITOR=nvim
 export ZVM_VI_EDITOR=nvim"
 zinit {{ get . "act" | default "light" }} neovim/neovim
+
+{{- else if eq $method "mise" }}
+
+{{- template "zshrc/helpers/h3" "Neovim" }}
+
+export EDITOR=nvim
+export ZVM_VI_EDITOR=nvim
+
+{{- end }}
 
 ## Setup vim alternative alias
 alias v="nvim"
